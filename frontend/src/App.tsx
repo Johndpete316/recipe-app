3cc52860a0d8781b167af262d976a39bd2c47c11
John Peterson <johndpete5316@outlook.com>
2023-05-05 12:19:37 -0400

    impliment react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './styles/App.css'
 
 import Navbar from './components/navbar';
 import RecipeList from "./components/RecipeList";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
-      <div className="logos">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <div className="card">
         <RecipeList token='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjVXUW1lbDlEWWFwN0FEWTVhUEhueSJ9.eyJpc3MiOiJodHRwczovL2Rldi0xMXZyNndjMy5hdXRoMC5jb20vIiwic3ViIjoiV1JjUVFvN1JOZDVQMGRSdHhYaHFkd3VKWlJ3YnNkcVZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcmVjaXBlYXBpLmRyaWxsY2hhbi5uZXQiLCJpYXQiOjE2ODI4MTE3NjEsImV4cCI6MTY4NTQwMzc2MSwiYXpwIjoiV1JjUVFvN1JOZDVQMGRSdHhYaHFkd3VKWlJ3YnNkcVYiLCJzY29wZSI6InJlYWQ6bWVzc2FnZXMgcmVhZDpyZWNpcGVzIHdyaXRlOmRhdGEiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.YzXrax4l5NSvGdi6Cy-y9nxDn4auOIa9QM4c_nc4c8qGzVKfmcr2yT07rATdlyec193c_JNNmwG2_bu9gBsmVcT6aqpkL4xhWoRnldaM35tprh7GdWMqi3uemSjj-3H-Ba7WlZT8PLGrT84fXRScdSWgfHvoN_OAOjtctH7Psk0kRkVeUJenHFXY66M37p0s9UZUUQkXSqJ-xju1E74qZi3NxLCGllqjbW0rWgclWWq_NQcFBG3rw1TbBzB9LUOAjTqalX35l73a6gMMEpKylz9vtCyjOoLNFR9hWAkHqG1xrLsbvPjrAWCvQjutozqmy4TZVHk-0lTtVyLhvOcAZQ' />
       </div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App.tsx'
 import 'semantic-ui-css/semantic.min.css';
 
 import { Auth0Provider } from '@auth0/auth0-react'
 import { domain, clientId } from '../config.json'
 
+import ErrorPage from './components/error-page'
+
 // address possible XSS issues with using localstorage
 // https://auth0.com/docs/libraries/auth0-single-page-app-sdk#change-storage-options
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -19,7 +33,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         redirect_uri: window.location.origin,
       }}
       >
-      <App />
+      <RouterProvider router={router} />
     </Auth0Provider>
   </React.StrictMode>,
 )
