2bfc160a5d79758b60e4e72ad6b844245106cef6
John Peterson <johndpete5316@outlook.com>
2023-05-05 11:39:56 -0400

    placeholder token

--- frontend/src/components/ReceipeImage.tsx
+++ frontend/src/components/ReceipeImage.tsx
@@ -4,6 +4,7 @@ import { Image } from 'semantic-ui-react';
 const RecipeImage: React.FC = () => {
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const placeholderImageUrl = '/api/placeholder';
+  // this is a placeholder token, replace with your actual token
   const authToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjVXUW1lbDlEWWFwN0FEWTVhUEhueSJ9.eyJpc3MiOiJodHRwczovL2Rldi0xMXZyNndjMy5hdXRoMC5jb20vIiwic3ViIjoiV1JjUVFvN1JOZDVQMGRSdHhYaHFkd3VKWlJ3YnNkcVZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcmVjaXBlYXBpLmRyaWxsY2hhbi5uZXQiLCJpYXQiOjE2ODI4MTE3NjEsImV4cCI6MTY4NTQwMzc2MSwiYXpwIjoiV1JjUVFvN1JOZDVQMGRSdHhYaHFkd3VKWlJ3YnNkcVYiLCJzY29wZSI6InJlYWQ6bWVzc2FnZXMgcmVhZDpyZWNpcGVzIHdyaXRlOmRhdGEiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.YzXrax4l5NSvGdi6Cy-y9nxDn4auOIa9QM4c_nc4c8qGzVKfmcr2yT07rATdlyec193c_JNNmwG2_bu9gBsmVcT6aqpkL4xhWoRnldaM35tprh7GdWMqi3uemSjj-3H-Ba7WlZT8PLGrT84fXRScdSWgfHvoN_OAOjtctH7Psk0kRkVeUJenHFXY66M37p0s9UZUUQkXSqJ-xju1E74qZi3NxLCGllqjbW0rWgclWWq_NQcFBG3rw1TbBzB9LUOAjTqalX35l73a6gMMEpKylz9vtCyjOoLNFR9hWAkHqG1xrLsbvPjrAWCvQjutozqmy4TZVHk-0lTtVyLhvOcAZQ'; // Replace with your actual authentication token
 
   useEffect(() => {
